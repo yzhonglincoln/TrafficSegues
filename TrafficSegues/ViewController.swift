2dca9ee1ed179294156279de9268f915d3366089
Soft Student <softdevstudent@robotics-mba-02.local>
2019-03-28 11:17:23 -0400

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Soft Dev Student on 2/26/19.
+//  Copyright © 2019 Alice Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+}
+
